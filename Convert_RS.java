331e829a22174a6d269a9d650b6fabedf7e66cef
Yash Bhapkar <142778656+Yash-7589@users.noreply.github.com>
2023-08-31 10:57:48 +0530

    Add files via upload

---
+++ Convert_RS.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Convert_RS {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter Dollers:");
+        int D=sc.nextInt();
+        System.out.println("Rupees equal to: "+(float)(D*82.65)+"RS");
+
+
+    }
+}
